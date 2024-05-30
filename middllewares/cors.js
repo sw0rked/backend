b90cdc51732079da94553f0e6ad7547fc212e32e
sw0rked <168303638+sw0rked@users.noreply.github.com>
2024-05-30 23:01:45 +0500

    Update cors.js

--- middllewares/cors.js
+++ middllewares/cors.js
@@ -1,13 +1,18 @@
-const allowedCors = ["https://localhost:3000", "https://localhost:3001",'https://pindie.portal.nomoredomainswork.ru','http://pindie.portal.nomoredomainswork.ru','https://portal.game.pindie.nomoredomainswork.ru','http://portal.game.pindie.nomoredomainswork.ru'];
+const allowedCors = [
+  'https://practicum.yandex.ru',
+  'https://students-projects.ru',
+  'sergeylf.nomorepartiesco.ru'
+];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
-}
-module.exports=cors;
+};
+
+module.exports = cors;
